33d790925b9634e0ebf15078bf5c39485901c4b0
Aleksandra <tkaczyk.aleksandra@outlook.com>
2021-03-19 17:36:20 +0100

    section contact and adding js script to close nav with click on any nav__link

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
+const navItem = document.querySelectorAll('.nav__link');
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active');
     navMobile.classList.toggle('nav-mobile--active');
+
+    navItem.forEach(item => {
+        item.addEventListener('click', () => {
+            navBtn.classList.remove('is-active');
+            navMobile.classList.remove('nav-mobile--active');
+        });
+    });
 }
 
 navBtn.addEventListener('click', handleNav);
